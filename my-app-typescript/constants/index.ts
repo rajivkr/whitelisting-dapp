f995cbe33c278112b8653be885fa9525764669f9
Rajiv Ramakrishnan <rajiv270992@gmail.com>
2022-12-22 16:53:09 +0530

    feat: first commit

---
+++ my-app-typescript/constants/index.ts
@@ -0,0 +1,68 @@
+export const WHITELIST_CONTRACT_ADDRESS =
+  '0xbCC0F2A717A94a2dBC5176CaEF3c1C5D8C780D8d';
+
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: 'uint8',
+        name: '_maxWhitelistedAddresses',
+        type: 'uint8',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    inputs: [],
+    name: 'addAddressToWhitelist',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'maxWhitelistedAddresses',
+    outputs: [
+      {
+        internalType: 'uint8',
+        name: '',
+        type: 'uint8',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'numAddressesWhitelisted',
+    outputs: [
+      {
+        internalType: 'uint8',
+        name: '',
+        type: 'uint8',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: '',
+        type: 'address',
+      },
+    ],
+    name: 'whitelistedAddresses',
+    outputs: [
+      {
+        internalType: 'bool',
+        name: '',
+        type: 'bool',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
