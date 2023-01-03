fd5f9f6fdab56685de3c595e2eccfe6e2f17f6ab
Rajiv Ramakrishnan <rajiv270992@gmail.com>
2023-01-03 15:25:39 +0530

    fix: adding return for provider

--- my-app-typescript/pages/index.tsx
+++ my-app-typescript/pages/index.tsx
@@ -89,7 +89,7 @@ export default function Home() {
     try {
       // Get the provider from web3Modal, which in our case is MetaMask
       // No need for the Signer here, as we are only reading state from the blockchain
-      const provider = await getProviderOrSigner();
+      const provider = (await getProviderOrSigner()) as providers.Web3Provider;
       // We connect to the Contract using a Provider, so we will only
       // have read-only access to the Contract
       const whitelistContract = new Contract(
